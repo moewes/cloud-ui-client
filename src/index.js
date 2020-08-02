99a7aac521062843052ec26ffc89ba0335b89370
maik <moewes@users.noreply.github.com>
2020-08-02 15:23:53 +0200

    import project

---
+++ src/index.js
@@ -0,0 +1,110 @@
+
+class OpenDxpPortlet extends HTMLElement {
+    constructor() {
+        super();
+        this.fields = new Map();
+    }
+
+    get backend() {
+        return this.getAttribute("backend")
+    }
+
+    connectedCallback() {
+        this.attachShadow({ mode: 'open' });
+        this.fetchApp();
+    }
+
+    disconnectedCallback() {
+        console.log("OpenDxpPortlet disconnecetCallback called");
+    }
+
+    fetchApp() {
+        return new Promise((res, rej) => {
+            fetch(this.backend ,{method: 'GET', headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'}
+            },)
+                .then(data => data.json())
+                .then((json) => {
+                    json.forEach(component => {
+                        console.log(component);
+                        this.renderElement(component, this.shadowRoot);
+                    });
+                })
+                .catch((error) => rej(error));
+        })
+    }
+
+    renderElement(component, parent) {
+        const item = document.createElement(component.tag);
+        item.id = component.id;
+        if (component.value) {
+            item.value = component.value;
+            item.addEventListener("input", (event) => {
+                console.log(event);
+                console.log(event.path[0].id);
+                const field = event.path[0];
+                this.fields.set(field.id, field.value);
+            });
+        }
+        if (component.innerHtml) {
+            item.innerHTML = component.innerHtml;
+        }
+        if (component.attributes) {
+            console.log('has attributes')
+        }
+        if (component.events) {
+            component.events.forEach((eventdef) => {
+                item.addEventListener(eventdef, (event) => {
+                    console.log(event);
+                    console.log(event.path[0].id);
+
+                    let message = { id: event.path[0].id, event: eventdef, fields: [] };
+                    this.fields.forEach((value, key) => {
+                        if (value) {
+                            let field = { name: key, value: value };
+                            message.fields.push(field);
+                        }
+                    });
+                    console.log(JSON.stringify(message));
+                    this.sendToApp(message);
+                });
+            });
+        }
+
+        parent.appendChild(item);
+
+        this.fields.set(component.id, component.value);
+        if (component.children) {
+            component.children.forEach(child => this.renderElement(child, item));
+        }
+    }
+
+    sendToApp(message) {
+        return new Promise((res, rej) => {
+            fetch(this.backend, 
+                {
+                    method: 'POST', headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(message)
+                })
+                .then(data => data.json())
+                .then((json) => {
+                    console.log(this.shadowRoot);
+                    this.shadowRoot.childNodes.forEach(item => {
+                        this.shadowRoot.removeChild(item);
+                    });
+                    console.log(this.shadowRoot);
+                    json.forEach(component => {
+                        console.log(component);
+                        this.renderElement(component, this.shadowRoot);
+                    });
+                })
+                .catch((error) => rej(error));
+        })
+    }
+}
+
+customElements.define('open-dxp-portlet', OpenDxpPortlet);
