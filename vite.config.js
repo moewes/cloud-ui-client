6f00ee51a0cf6529dc9988a0daacdf821664619f
Maik <moewes@users.noreply.github.com>
2023-06-10 15:52:04 +0200

    migrate building to vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: "target/classes/META-INF/resources/webjars/cloudui-client/0.4.0",
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'cloudui-client-lib',
+      // the proper extensions will be added
+      fileName: 'cloudui-client-lib',
+    },
+  },
+})
